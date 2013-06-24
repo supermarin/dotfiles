4d439809eff1752b11451114cafea4ba7fd781b0
Marin Usalj <mneorr@gmail.com>
2013-06-24 16:06:29 +0200

    added homebrew pip and rbenv support

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,21 +1,46 @@
 set fish_greeting ""
-alias steve=jobs
-alias please=sudo
-alias simon-sez=sudo
+
+set PATH /usr/local/bin /usr/bin /bin /usr/sbin /sbin
+
+### pip
+set PATH $HOME/Library/Python/2.7/bin $PATH
+
+### Homebrew
+set PATH $HOME/usr/local/bin $PATH
+
+### rbenv
+set PATH $HOME/.rbenv/bin $PATH
+set PATH $HOME/.rbenv/shims $PATH
+rbenv rehash >/dev/null ^&1
+
+
+### bundler
+
+function be
+  bundle exec $argv
+end
+
+function bi
+  bundle install
+end
+
+function bu
+  bundle update
+end
+
+### aliases
 alias la="ls -lah"
-alias ..l="cd .. ; ls -lah"
 alias findproccess="ps ax |grep -i"
 alias stupidXcode="rm -rf ~/Library/Developer/Xcode/DerivedData"
 alias apacheSites="cd /Applications/XAMPP/xamppfiles/htdocs/"
-alias mp="md5 -qs"
 alias duh="du -hc"
 alias ccat='pygmentize -g'
 
-# Git
+### Git
 alias gl="git l"
 alias gp="git push"
 alias gl="git pull --rebase"
 alias gs="git status -sb"
 
-# Gem development
+### Gem development
 alias pod-dev='COCOA_PODS_ENV=development ~/code/OSS/CocoaPods/CocoaPods/bin/pod'
