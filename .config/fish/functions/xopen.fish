5394c62d1125a7fd9e204d729b51ecbde5cc5829
Marin Usalj <mneorr@gmail.com>
2014-03-04 17:22:54 -0800

    augment xopen to handle non-cocoapods projects

--- .config/fish/functions/xopen.fish
+++ .config/fish/functions/xopen.fish
@@ -1,4 +1,10 @@
 function xopen
-    open (find . -name '*.xcworkspace' -maxdepth 1)
+    open (find . -name '*.xcworkspace' -maxdepth 1) 2> /dev/null
+    if [ !$status ]
+        open (find . -name '*.xcodeproj' -maxdepth 1) 2> /dev/null
+    end
+    if [ !$status ]
+        echo "No .xcworkspace or .xcodeproj found in the working directory."
+    end
 end
 
