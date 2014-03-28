53f0213884769c7e8c4329708e0e92ccb4b60604
Marin Usalj <mneorr@gmail.com>
2014-03-28 13:40:24 -0700

    Fix incorrect behavior on xopen

--- .config/fish/functions/xopen.fish
+++ .config/fish/functions/xopen.fish
@@ -1,10 +1,11 @@
 function xopen
     open (find . -name '*.xcworkspace' -maxdepth 1) 2> /dev/null
-    if [ !$status ]
+    if [ $status -ne 0 ]
         open (find . -name '*.xcodeproj' -maxdepth 1) 2> /dev/null
-    end
-    if [ !$status ]
-        echo "No .xcworkspace or .xcodeproj found in the working directory."
+
+        if [ $status -ne 0 ]
+            echo "No .xcworkspace or .xcodeproj found in the working directory."
+        end
     end
 end
 
