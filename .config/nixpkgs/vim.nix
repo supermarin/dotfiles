985740f16cbb394b11dadc841eb280aa6a859520
Marin Usalj <marin2211@gmail.com>
2020-12-04 11:30:10 +0300

    bulk commit of somewhat working neovim

--- .config/nixpkgs/vim.nix
+++ .config/nixpkgs/vim.nix
@@ -5,25 +5,30 @@ pkgs:
   vimdiffAlias = true;
   viAlias = true;
   withPython3 = true;
+  package = pkgs.neovim-nightly;
   plugins = with pkgs.vimPlugins; [
+    completion-nvim 
     fzf-vim
     fzfWrapper
     gruvbox
+    nvim-lspconfig
     vim-commentary
+    vim-fish
+    vim-nix
     vim-repeat
     vim-surround
-    vim-nix
   ];
   extraConfig = ''
-    set ts=2 sw=2 expandtab
-    set smartcase
-    set ignorecase
-    set smartindent
-    set grepprg=rg\ --vimgrep
-    set splitright
-    set splitbelow
-    set mouse=nv
     let mapleader=" "
+    set clipboard=unnamedplus
+    set grepprg=rg\ --vimgrep
+    set ignorecase
+    set mouse=a
+    set smartcase
+    set smartindent
+    set splitbelow
+    set splitright
+    set ts=2 sw=2 expandtab
 
     color gruvbox
 
@@ -31,9 +36,20 @@ pkgs:
     nnoremap gl $
     nnoremap <leader>p :GitFiles<cr>
     nnoremap <leader>b :Buffers<cr>
+    nnoremap <leader>f :FZF<cr>
 
     " experimental: neovim terminal
     tnoremap <Esc> <C-\><C-n>
     au CursorHold,CursorHoldI * checktime
+
+    let g:LanguageClient_serverCommands = {
+    \ 'nix': ['rnix-lsp'],
+    \ 'python': ['python-language-server'],
+    \ 'rust': ['rls']
+    \ }
+
+    lua <<EOF
+    ${builtins.readFile ./nvim-lsp.lua}
+    EOF
   '';
 }
