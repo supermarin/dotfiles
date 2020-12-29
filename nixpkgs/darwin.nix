68072307ceef0d3da6c0764f1f65a767608fe728
Marin Usalj <marin2211@gmail.com>
2020-12-29 16:09:54 -0500

    remove a bunch of shit out.
    This doesn't work at all with fish.
    Will keep stuff in home-manager for now and keep only macOS specific
    conf here.

--- nixpkgs/darwin.nix
+++ nixpkgs/darwin.nix
@@ -1,34 +1,22 @@
 { config, pkgs, ... }:
 
 {
-  environment.darwinConfig = "$HOME/.config/nixpkgs/darwin.nix";
-  environment.variables = {
-    EDITOR = "vim";
-  };
-  environment.shells = with pkgs; [
-    bashInteractive_5
-    fish
-  ];
-  environment.systemPackages = with pkgs; [ 
-    home-manager
-    vim
-    git
-  ];
-
-  # Auto upgrade nix package and the daemon service.
-  # services.nix-daemon.enable = true;
-  # nix.package = pkgs.nix;
-
-  # Create /etc/bashrc that loads the nix-darwin environment.
-  programs.fish.enable = true;
-
   users.users.supermarin = {
-    shell = pkgs.fish;
+    shell = pkgs.bash;
   };
+
+  # Darwin only below. TODO: move to separate file
+  system.defaults.finder.QuitMenuItem = true;
+  system.defaults.dock.orientation = "right";
+  system.keyboard.enableKeyMapping = true;
+  system.keyboard.remapCapsLockToControl = true;
 
   # Used for backwards compatibility, please read the changelog before changing.
   # $ darwin-rebuild changelog
   system.stateVersion = 4;
-  system.keyboard.enableKeyMapping = true;
-  system.keyboard.remapCapsLockToControl = true;
+
+  # Auto upgrade nix package and the daemon service.
+  # services.nix-daemon.enable = true;
+  # nix.package = pkgs.nix;
+  environment.darwinConfig = "$HOME/.config/nixpkgs/darwin.nix";
 }
