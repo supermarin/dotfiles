489eba2a5a662778f579d21c364606b76dfca3f4
Marin Usalj <marin2211@gmail.com>
2021-01-10 11:58:04 -0500

    [vim] add vim-lsp and gopls settings

--- nixpkgs/vim.nix
+++ nixpkgs/vim.nix
@@ -9,6 +9,7 @@ pkgs:
     vim-commentary
     vim-fish
     vim-gitgutter
+    vim-lsp
     vim-nix
     vim-repeat
     vim-surround
@@ -41,5 +42,47 @@ pkgs:
     nnoremap <leader>f :FZF<cr>
 
     au CursorHold,CursorHoldI * checktime
+
+
+    """""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+    " LSP
+    """"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""" 
+    function! s:on_lsp_buffer_enabled() abort
+      setlocal omnifunc=lsp#complete
+      setlocal signcolumn=yes
+      if exists('+tagfunc') | setlocal tagfunc=lsp#tagfunc | endif
+      nmap <buffer> gd <plug>(lsp-definition)
+      nmap <buffer> gr <plug>(lsp-references)
+      nmap <buffer> gi <plug>(lsp-implementation)
+      nmap <buffer> gD <plug>(lsp-type-definition)
+      nmap <buffer> [g <Plug>(lsp-previous-diagnostic)
+      nmap <buffer> ]g <Plug>(lsp-next-diagnostic)
+      nmap <buffer> K <plug>(lsp-hover)
+
+      nmap <buffer> <leader>rn <plug>(lsp-rename)
+      nmap <buffer> <leader>ca <plug>(lsp-code-action)
+      nmap <buffer> <leader>dd <plug>(lsp-document-diagnostics)
+      nmap <buffer> <leader>df <plug>(lsp-document-format)
+
+      let g:lsp_format_sync_timeout = 1000
+      autocmd! BufWritePre *.rs,*.go call execute('LspDocumentFormatSync')
+
+      " refer to doc to add more commands
+    endfunction
+
+    augroup lsp_install
+      au!
+      autocmd User lsp_buffer_enabled call s:on_lsp_buffer_enabled()
+    augroup END
+
+    if executable('gopls')
+      au User lsp_setup call lsp#register_server({
+          \ 'name': 'gopls',
+          \ 'cmd': {server_info->['gopls']},
+          \ 'allowlist': ['go'],
+          \ })
+    else
+      echo("gopls not found!??")
+    endif
   '';
 }
