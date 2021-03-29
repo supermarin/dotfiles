bbb2b8d2a75262e6f91f95affee6b4f8e48cdffd
Marin Usalj <marin2211@gmail.com>
2021-03-28 20:30:41 -0400

    remove a bunch of alacritty color settings, font 10

--- alacritty.nix
+++ alacritty.nix
@@ -5,7 +5,7 @@
       TERM = "xterm-256color";
     };
     font = {
-      size = 11;
+      size = 10;
     };
     window = {
       padding = {
@@ -20,34 +20,5 @@
       { key = "Minus";      mods = "Control";     action = "DecreaseFontSize"; }
     ];
     draw_bold_text_with_bright_colors = true;
-    extraConfig = ''
-      colors:
-        # Default colors
-        primary:
-          background: '0x282828'
-          foreground: '0xeeeeee'
-
-        # Normal colors
-        normal:
-          black:   '0x282828'
-          red:     '0xf43753'
-          green:   '0xc9d05c'
-          yellow:  '0xffc24b'
-          blue:    '0xb3deef'
-          magenta: '0xd3b987'
-          cyan:    '0x73cef4'
-          white:   '0xeeeeee'
-
-        # Bright colors
-        bright:
-          black:   '0x4c4c4c'
-          red:     '0xf43753'
-          green:   '0xc9d05c'
-          yellow:  '0xffc24b'
-          blue:    '0xb3deef'
-          magenta: '0xd3b987'
-          cyan:    '0x73cef4'
-          white:   '0xfeffff'
-    '';
   };
 }
