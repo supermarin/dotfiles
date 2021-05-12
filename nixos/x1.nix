58f85f5308947ccf6f684beffa0a76585aeeb037
Marin Usalj <marin2211@gmail.com>
2021-05-12 10:48:54 +0200

    [not really tested] add laptop power management

--- nixos/x1.nix
+++ nixos/x1.nix
@@ -1,12 +1,13 @@
-# Usage:
-#   $ ln -s $0 /etc/nixos/configuration.nix
-# Then just normally
-#   $ nixos-rebuild switch
-
 { config, pkgs, ... }:
 {
   imports = [
       ./hardware-x1.nix
       (import ./configuration.nix { hostname = "tokio"; config = config; pkgs = pkgs; })
   ];
+  boot.kernelParams = [
+    "mem_sleep_default=deep"
+  ];
+  services.fwupd.enable = true;
+  # For thinkpad
+  services.tlp.enable = true;
 }
