0488a7685e913e3ef11fdc993dc8476e6e3299fd
Marin Usalj <marin2211@gmail.com>
2021-05-23 11:42:33 +0200

    Pin home-manager and neovim

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -3,7 +3,7 @@
 {
   imports = [
       (import "${builtins.fetchTarball 
-        https://github.com/rycee/home-manager/archive/master.tar.gz}/nixos")
+        https://github.com/rycee/home-manager/archive/64607f58b75741470284c698f82f0199fcecdfa7.tar.gz}/nixos")
   ];
 
   boot.kernelPackages = pkgs.linuxPackages_5_11;
