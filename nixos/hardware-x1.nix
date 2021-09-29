f8839db2ea1a83f5e306f7f69e7d43855d895de9
Marin Usalj <marin2211@gmail.com>
2021-09-29 15:23:03 +0200

    clean up hardware files

--- nixos/hardware-x1.nix
+++ nixos/hardware-x1.nix
@@ -4,32 +4,28 @@
 { config, lib, pkgs, modulesPath, ... }:
 
 {
-  imports =
-    [ (modulesPath + "/installer/scan/not-detected.nix")
-    ];
+  imports = [ 
+    (modulesPath + "/installer/scan/not-detected.nix")
+  ];
 
   boot.initrd.availableKernelModules = [ "xhci_pci" "thunderbolt" "nvme" "usb_storage" "sd_mod" ];
-  boot.initrd.kernelModules = [ ];
-  boot.kernelModules = [ "kvm-intel" ];
-  boot.extraModulePackages = [ ];
-
-  fileSystems."/" =
-    { device = "/dev/disk/by-uuid/a8b013cc-3a89-410c-9e1f-e8086b304f74";
-      fsType = "ext4";
-    };
-
   boot.initrd.luks.devices.cryptroot.device = "/dev/disk/by-uuid/b40ce794-595a-4057-a7bf-61a4a0e4371f";
+  boot.kernelModules = [ "kvm-intel" ];
 
-  fileSystems."/boot" =
-    { device = "/dev/disk/by-uuid/FE77-508B";
-      fsType = "vfat";
-    };
+  fileSystems."/" = { 
+    device = "/dev/disk/by-uuid/a8b013cc-3a89-410c-9e1f-e8086b304f74";
+    fsType = "ext4";
+  };
 
-  swapDevices =
-    [ { device = "/dev/disk/by-uuid/1f3fc21a-3780-4f6a-a8ec-691fb9070e6b"; }
-    ];
+  fileSystems."/boot" = { 
+    device = "/dev/disk/by-uuid/FE77-508B";
+    fsType = "vfat";
+  };
 
-  powerManagement.cpuFreqGovernor = lib.mkDefault "powersave";
-  # high-resolution display
+  swapDevices = [
+    { device = "/dev/disk/by-uuid/1f3fc21a-3780-4f6a-a8ec-691fb9070e6b"; }
+  ];
+
+  powerManagement.cpuFreqGovernor = lib.mkDefault "powersave"; # TODO: should we remove this?
   hardware.video.hidpi.enable = lib.mkDefault true;
 }
