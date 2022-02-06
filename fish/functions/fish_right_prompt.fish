6318d2f633176f230cc99f1a242f2c5ab1a8c37a
Marin Usalj <marin2211@gmail.com>
2022-02-06 18:09:40 -0400

    add notification if cmd takes more than 30s

--- fish/functions/fish_right_prompt.fish
+++ fish/functions/fish_right_prompt.fish
@@ -1,3 +1,14 @@
-set -l right_color (set_color brown)
-echo $right_color $PWD | awk -F/ '{print $1 $(NF-1) "/" $(NF)}'
-set_color normal
+function fish_right_prompt
+    if test $CMD_DURATION
+      # Show notification if dration is more than 30 seconds
+      if test $CMD_DURATION -gt 30000
+          # Show duration of the last command in seconds
+          set duration (echo "$CMD_DURATION 1000" | awk '{printf "%.3fs", $1 / $2}')
+          notify-send (echo (history | head -1) returned $status after $duration)
+      end
+    end
+
+    set -l right_color (set_color brown)
+    echo $right_color $PWD | awk -F/ '{print $1 $(NF-1) "/" $(NF)}'
+    set_color normal
+end
