a5b2b70b17ede80beda98ffa4a8c8c72c9dca90c
Marin Usalj <marin2211@gmail.com>
2022-02-06 21:50:34 -0400

    [fish] only notify if terminal is not focused

--- fish/functions/fish_right_prompt.fish
+++ fish/functions/fish_right_prompt.fish
@@ -2,9 +2,14 @@ function fish_right_prompt
     if test $CMD_DURATION
       # Show notification if dration is more than 30 seconds
       if test $CMD_DURATION -gt 30000
+        # Only notify if terminal is not active
+        set -l active (swaymsg -t get_tree | jq -r '..|try select(.focused == true)' | jq '.app_id')
+        if test $active != '"foot"'
           # Show duration of the last command in seconds
-          set duration (echo "$CMD_DURATION 1000" | awk '{printf "%.3fs", $1 / $2}')
-          notify-send (echo (history | head -1) returned $status after $duration)
+          set -l duration (echo "$CMD_DURATION 1000" | awk '{printf "%.3fs", $1 / $2}')
+          set -l title (history | head -1)
+          notify-send $title (echo $title returned $status after $duration)
+        end
       end
     end
 
