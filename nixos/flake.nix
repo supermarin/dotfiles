20b4e7723c53ba874d9699e4008b43cc6e63bf85
Marin Usalj <marin2211@gmail.com>
2022-06-24 15:48:45 -0400

    update flake sources

--- nixos/flake.nix
+++ nixos/flake.nix
@@ -74,7 +74,7 @@
           home-manager.darwinModules.home-manager {
             home-manager.useGlobalPkgs = true;
             # home-manager.useUserPackages = true;
-            home-manager.users.supermarin = import ../home.nix;            
+            home-manager.users.supermarin = import ../home.nix;
           }
         ];
       };
