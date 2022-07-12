6da8104d14492ba5c1475f17a6b5de4ef0451dcf
Marin Usalj <marin2211@gmail.com>
2022-07-12 16:57:19 -0400

    [nixos, flake] pin to a specific commit explicitly
    Unfortunately, if we bump too far ahead, some of the deps haven't been
    built and cached on hydra. This results in the machine building like a
    maniac and I'm not too excited about that.

--- nixos/flake.nix
+++ nixos/flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    nixpkgs.url = github:nixos/nixpkgs;
+    nixpkgs.url = github:nixos/nixpkgs/c869aa9ac9553c49ee7dff9ac7f8f510b1e8d5f7;
     home-manager.url = github:nix-community/home-manager;
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
     darwin.url = github:lnl7/nix-darwin;
@@ -63,6 +63,56 @@
           ./vpn-configuration.nix
         ];
         specialArgs = { hostname = "vpn"; };
+      };
+
+      butters = nixpkgs.lib.nixosSystem {
+        system = "aarch64-linux";
+        modules = [
+          {
+            boot.loader.efi.canTouchEfiVariables = true;
+            boot.loader.grub = {
+              enable = true;
+              device = "nodev";
+              efiSupport = true;
+            };
+            # environment.systemPackages = with pkgs; [
+            #   file # file(1)
+            #   firefox
+            #   killall # killall(1)
+            #   unzip
+            # ];
+            hardware.pulseaudio.enable = true;
+            hardware.bluetooth.enable = true; # enables bluez
+            programs.ssh.startAgent = true;
+            sound.enable = true;
+            services.openssh.enable = true;
+            nix = {
+              settings = {
+                trusted-users = [ "supermarin" ]; # enable nix-copy-closure
+              };
+              extraOptions = ''
+              experimental-features = nix-command flakes
+              '';
+              gc = {
+                automatic = true;
+                dates = "monthly";
+              };
+              optimise = {
+                automatic = true;
+                dates = [ "monthly" ];
+              };
+            };
+            time.timeZone = "America/NewYork";
+            users.users.supermarin = {
+              # shell = pkgs.fish;
+              isNormalUser = true;
+              extraGroups = [ "wheel" ]; 
+              openssh.authorizedKeys.keys = [
+                "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPx9yl0N1u8n7nO3uZilfOGa/MtyFTfHsEgs8MDGAnAL supermarin@tokio"
+              ];
+            };
+          }
+        ];
       };
     };
     
