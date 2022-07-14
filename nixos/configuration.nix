92e6ebda596a5bf8c78efaf009d7076ed96ebbdb
Marin Usalj <marin2211@gmail.com>
2022-07-14 13:23:12 -0400

    [sway] second attempt to ditch gnome
    remove endless dconf settings.
    Add some boilerplate from https://nixos.wiki/wiki/Sway.
    It's supposed to fix GTK theming, and also (hopefully) the annoying
    BT keyboard lag. Right now I'm not experiencing it.
    Will try to remove the DBUS stuff and try again

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -1,4 +1,44 @@
 { hostname, pkgs, ... }:
+let
+# bash script to let dbus know about important env variables and
+  # propogate them to relevent services run at the end of sway config
+  # see
+  # https://github.com/emersion/xdg-desktop-portal-wlr/wiki/"It-doesn't-work"-Troubleshooting-Checklist
+  # note: this is pretty much the same as  /etc/sway/config.d/nixos.conf but also restarts  
+  # some user services to make sure they have the correct environment variables
+  dbus-sway-environment = pkgs.writeTextFile {
+    name = "dbus-sway-environment";
+    destination = "/bin/dbus-sway-environment";
+    executable = true;
+
+    text = ''
+  dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP=sway
+  systemctl --user stop pipewire pipewire-media-session xdg-desktop-portal xdg-desktop-portal-wlr
+  systemctl --user start pipewire pipewire-media-session xdg-desktop-portal xdg-desktop-portal-wlr
+      '';
+  };
+
+  # currently, there is some friction between sway and gtk:
+  # https://github.com/swaywm/sway/wiki/GTK-3-settings-on-Wayland
+  # the suggested way to set gtk settings is with gsettings
+  # for gsettings to work, we need to tell it where the schemas are
+  # using the XDG_DATA_DIR environment variable
+  # run at the end of sway config
+  configure-gtk = pkgs.writeTextFile {
+      name = "configure-gtk";
+      destination = "/bin/configure-gtk";
+      executable = true;
+      text = let
+        schema = pkgs.gsettings-desktop-schemas;
+        datadir = "${schema}/share/gsettings-schemas/${schema.name}";
+      in ''
+        export XDG_DATA_DIRS=${datadir}:$XDG_DATA_DIRS
+        gnome_schema=org.gnome.desktop.interface
+        gsettings set $gnome_schema gtk-theme 'Dracula'
+        '';
+  };
+
+in
 {
   boot.loader.efi.canTouchEfiVariables = true;
   boot.loader.grub = {
@@ -80,6 +120,8 @@
   # Only put system software in here, e.g. stuff that is installed by
   # default on macOS and Ubuntu. The user software goes in home.nix.
   environment.systemPackages = with pkgs; [
+    configure-gtk
+    dbus-sway-environment
     file # file(1)
     firefox
     killall # killall(1)
@@ -119,20 +161,7 @@
       export QT_WAYLAND_DISABLE_WINDOWDECORATION="1"
     '';
   };
-  # environment.loginShellInit = ''
-  #   if [ -z $DISPLAY ] && [ "$(tty)" = "/dev/tty1" ]; then
-  #     exec sway
-  #   fi
-  # '';
 
-  services.xserver.enable = true;
-  services.xserver.displayManager.gdm.enable = true;
-  services.xserver.desktopManager.gnome.enable = true;
-  environment.gnome.excludePackages = with pkgs; [ 
-    epiphany gnome.tali gnome.iagno gnome.hitori gnome.atomix gnome-tour
-    gnome.gnome-music
-  ];
-              
   fonts = {
     enableDefaultFonts = false;
     fonts = with pkgs; [
