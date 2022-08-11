7b2b87eac98061d069c1bf13f535c9116f01cba7
Marin Usalj <marin2211@gmail.com>
2022-08-10 20:49:05 -0400

    [nixos] remove unused sh8

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -1,44 +1,4 @@
 { hostname, pkgs, ... }:
-let
-# bash script to let dbus know about important env variables and
-  # propogate them to relevent services run at the end of sway config
-  # see
-  # https://github.com/emersion/xdg-desktop-portal-wlr/wiki/"It-doesn't-work"-Troubleshooting-Checklist
-  # note: this is pretty much the same as  /etc/sway/config.d/nixos.conf but also restarts  
-  # some user services to make sure they have the correct environment variables
-  dbus-sway-environment = pkgs.writeTextFile {
-    name = "dbus-sway-environment";
-    destination = "/bin/dbus-sway-environment";
-    executable = true;
-
-    text = ''
-  dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP=sway
-  systemctl --user stop pipewire pipewire-media-session xdg-desktop-portal xdg-desktop-portal-wlr
-  systemctl --user start pipewire pipewire-media-session xdg-desktop-portal xdg-desktop-portal-wlr
-      '';
-  };
-
-  # currently, there is some friction between sway and gtk:
-  # https://github.com/swaywm/sway/wiki/GTK-3-settings-on-Wayland
-  # the suggested way to set gtk settings is with gsettings
-  # for gsettings to work, we need to tell it where the schemas are
-  # using the XDG_DATA_DIR environment variable
-  # run at the end of sway config
-  configure-gtk = pkgs.writeTextFile {
-      name = "configure-gtk";
-      destination = "/bin/configure-gtk";
-      executable = true;
-      text = let
-        schema = pkgs.gsettings-desktop-schemas;
-        datadir = "${schema}/share/gsettings-schemas/${schema.name}";
-      in ''
-        export XDG_DATA_DIRS=${datadir}:$XDG_DATA_DIRS
-        gnome_schema=org.gnome.desktop.interface
-        gsettings set $gnome_schema gtk-theme 'Dracula'
-        '';
-  };
-
-in
 {
   boot.loader.efi.canTouchEfiVariables = true;
   boot.loader.grub = {
@@ -69,7 +29,6 @@ in
     wg-quick.interfaces = {
       wg0 = {
         address = [ "10.100.0.4/24" ];
-        dns = [ "1.1.1.1" ];
         privateKeyFile = "/wg/private";
         listenPort = 51820;
         peers = [
@@ -123,8 +82,6 @@ in
   # Only put system software in here, e.g. stuff that is installed by
   # default on macOS and Ubuntu. The user software goes in home.nix.
   environment.systemPackages = with pkgs; [
-    configure-gtk
-    dbus-sway-environment
     file # file(1)
     firefox
     killall # killall(1)
