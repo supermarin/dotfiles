8a693f244e1ec551da080c30137811f74eb65dc5
Marin Usalj <marin2211@gmail.com>
2022-08-14 10:58:19 -0400

    Add simba's key to tokio's allowed

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -70,6 +70,7 @@
     extraGroups = [ "wheel" "networkmanager" "libvirtd" "docker" ]; 
     openssh.authorizedKeys.keys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPx9yl0N1u8n7nO3uZilfOGa/MtyFTfHsEgs8MDGAnAL supermarin@tokio"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHEStWVGTSqu2acHbyOaiDfMvnzg5AGi7FtZOQrbG7gB git@mar.in" # simba
     ];
   };
 
