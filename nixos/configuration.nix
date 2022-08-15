506adc8ccc1a7e31107b7ce8335c02477fe239f8
Marin Usalj <marin2211@gmail.com>
2022-08-15 11:15:53 -0400

    [nixos] remove GNOME

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -92,15 +92,6 @@
     virt-manager
   ];
 
-  # GNOME - TODO: remove this
-  services.xserver.enable = true;
-  services.xserver.displayManager.gdm.enable = true;
-  services.xserver.desktopManager.gnome.enable = true;
-  environment.gnome.excludePackages = with pkgs; [ 
-    epiphany gnome.tali gnome.iagno gnome.hitori gnome.atomix gnome-tour
-    gnome.gnome-music
-  ];
-
   # only for sway
   programs.sway = {
     enable = true;
