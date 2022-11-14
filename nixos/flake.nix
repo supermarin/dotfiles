aadc50d2c247e859371b655b0b3a44f7b6ec40d5
Marin Usalj <marin2211@gmail.com>
2022-11-14 17:05:20 +0100

    [flake] update inputs

--- nixos/flake.nix
+++ nixos/flake.nix
@@ -2,7 +2,7 @@
   inputs = {
     nixpkgs.url =
       "github:nixos/nixpkgs/104e7ab416917721b27b8815c3d68969b4dacd29";
-    home-manager.url = "github:supermarin/home-manager";
+    home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
     darwin.url = "github:lnl7/nix-darwin";
     darwin.inputs.nixpkgs.follows = "nixpkgs";
