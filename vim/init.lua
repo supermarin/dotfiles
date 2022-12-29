d6285620957165dbdc780ad60f870a22cd3f9312
Marin <git@mar.in>
2022-12-29 11:30:27 -0500

    [vim] back to sync formatting.
    can't save files for shit when using async

--- vim/init.lua
+++ vim/init.lua
@@ -37,7 +37,7 @@ local au = vim.api.nvim_create_augroup('YO_OY', { clear = true })
 vim.api.nvim_create_autocmd('BufWritePre', {
   pattern  = { '*.go', '*.lua', '*.nix', '*.rb', '*.py' },
   callback = function()
-    vim.lsp.buf.format({ async = true })
+    vim.lsp.buf.format(nil, 1000) -- async can't quit vim if files are changed
   end,
   group    = au,
 })
