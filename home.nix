bbeb3e9c3e21b4d7b453f246e360b7d352f0f7ef
Marin <git@mar.in>
2022-12-31 15:43:00 -0500

    [kitty] fix crash: don't remember window size
    This crashes fractional scaling on wayland

--- home.nix
+++ home.nix
@@ -69,6 +69,9 @@ in
       name = "JetBrains Mono";
       size = 12;
     };
+    settings = {
+      remember_window_size = false;
+    };
   };
 
   services = mkIf isLinux (import ./home-services.nix pkgs).services;
