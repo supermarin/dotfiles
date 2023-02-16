9ef98a6a48cbbfc6b62b2dbd07b906556bea9c2d
Marin <git@mar.in>
2023-02-16 09:34:14 -0500

    [home] return brave

--- home.nix
+++ home.nix
@@ -52,7 +52,7 @@ in
   ]
   ++ lib.optionals isDarwin [ ]
   ++ lib.optionals isLinux [
-    # brave # https://github.com/NixOS/nixpkgs/pull/98853/files
+    brave # https://github.com/NixOS/nixpkgs/pull/98853/files
     calcurse # calendar
     # calibre # books. Unsupported on aarch64-darwin as of Aug 10 2022
     fractal # matrix. Unsupported on aarch64-darwin as of Aug 10 2022 (libhandy)
