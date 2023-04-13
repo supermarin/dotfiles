f423778ff088b70c376f5ea42b7bf3f310125335
Marin <git@mar.in>
2023-04-13 14:19:05 -0400

    wat

---
+++ secrets/test.nix
@@ -0,0 +1,9 @@
+age-encryption.org/v1
+-> piv-p256 Xj1mVg A9xcYXG0Ew03dY/YG6sPxAuQiwNdnWXSv1cnZ4fGqPyR
+Vv9u0rL7Ptd4Iexxxqt51qKQ+cui0gA9gvisZPbpBPg
+-> piv-p256 b01zhg As3Zhdq8X5NAyZjsaJ6att/Ea3dB7SY7X1jqW6RP6LhU
+Y9A0GFVU1c2pCspD5Tr7wQ2CBagcueCBhERl5SWTmz0
+-> piv-p256 hJlN5Q AyfXWcnh8P/yUei/mmAFJgr4ZpcV0zmJsr/S7uFvHKE3
+AUW4T4swZThkf0OxuazD33MTN5G5Tzl6MLLWTyjao60
+--- rlbWbRVnkJ+oPt4fNYPWAsK3rWYaXm5JGPRq7ROJO1w
+ :3g$Ò’AI™éñ…øÈe¼vkJP|põžÚR§å™ŽóÖ÷Øð‚Zq+¥ßu¼¶
