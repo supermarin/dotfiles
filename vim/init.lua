49e05b87b0c178b5f986dcdf411638c4d791711d
Marin <git@mar.in>
2023-04-13 14:38:42 -0400

    [vim] add dp and dP mappings
    For consistency: cip, cap, dip, dap already work inside a paragraph.
    TODO: see if cp and cP make sense as well.

--- vim/init.lua
+++ vim/init.lua
@@ -1,6 +1,8 @@
 vim.g.mapleader = ' '
 vim.keymap.set('n', 'gh', '^')
 vim.keymap.set('n', 'gl', '$')
+vim.keymap.set('n', 'dp', 'd}')                  -- delete to end of paragraph
+vim.keymap.set('n', 'dP', 'd{')                  -- delete to beginning of paragraph
 vim.keymap.set('n', '<cr>', ':noh<cr>')
 vim.keymap.set('v', 'p', '"_dP') -- Unfuck paste in visual mode
 vim.keymap.set('t', '<Esc>', '<C-\\><C-n>') -- Escape in terminal
