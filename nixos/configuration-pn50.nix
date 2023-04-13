ca8ff450f791d176b141719a00cdfdeade643b78
Marin <git@mar.in>
2023-04-13 18:51:28 -0400

    tailscale on pumba

--- nixos/configuration-pn50.nix
+++ nixos/configuration-pn50.nix
@@ -25,6 +25,35 @@
     networkmanager.enable = true;
   };
 
+  services.tailscale.enable = true;
+  # create a oneshot job to authenticate to Tailscale
+  systemd.services.tailscale-autoconnect = {
+    description = "Automatic connection to Tailscale";
+
+    # make sure tailscale is running before trying to connect to tailscale
+    after = [ "network-pre.target" "tailscaled.service" ];
+    wants = [ "network-pre.target" "tailscaled.service" ];
+    wantedBy = [ "multi-user.target" ];
+
+    # set this service as a oneshot job
+    serviceConfig.Type = "oneshot";
+
+    # have the job run this shell script
+    script = with pkgs; ''
+      # wait for tailscaled to settle
+      sleep 2
+
+      # check if we are already authenticated to tailscale
+      status="$(${tailscale}/bin/tailscale status -json | ${jq}/bin/jq -r .BackendState)"
+      if [ $status = "Running" ]; then # if so, then do nothing
+        exit 0
+      fi
+
+      # otherwise authenticate with tailscale
+      ${tailscale}/bin/tailscale up -authkey ${secrets.tailscale.pumba.authkey}
+    '';
+  };
+
   services.syncthing = secrets.syncthing "pumba" // {
     extraOptions.options.gui.enabled = false;
     user = "marin";
