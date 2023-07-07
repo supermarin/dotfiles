81f3c637250805dd4f6588ba4016dbc7a65d47d2
Marin <git@mar.in>
2023-07-07 16:54:03 -0400

    readd

---
+++ secrets/vpn.nix
@@ -0,0 +1,12 @@
+age-encryption.org/v1
+-> piv-p256 Xj1mVg A0RdKEahZsq/lwSALR/wBx37x6z4EueAaRmKBNMc0ggp
+X87x56cEoaqXBimDbIEv4XQlm1DjX3n29S/deF3/tv8
+-> piv-p256 b01zhg Aqp2FF7vrcirHXlNNpcHxuieC1CdVFZMEA1VUZJdpjHE
+PaYMDqWipsslOHjPVCQFMCyETdeKXnnekZgwsys9CGA
+-> piv-p256 hJlN5Q AxMOaWqbsBt6b/UA1s+2W+Lk4eBLI0L6s8KG4HylnS5m
+iUkxidXFpE9DbBVKLWWEYp5wc761m8Ux9RkrLNxUWHQ
+--- y0VOAaRudyPXlDzgDM352rI/8UAehu4+Z/Kuh13wJoM
+p|—8w48{Ø¸/2éš/™J\Sµ%~¶Pš	¼·›Ëü¬í€»É
+ÕÜ±e/HÖ	YÚ xaèëÚÆUBÖ¿Î(À€ÅmøÀ”Ù°²]KÙ²4îÚ¨sMtþæ‘ö™5¦éMOKxQxzáHûLÃ¦l<ïmA…(peî'æe›Æˆ..úžôš®šø_²ÓoŠÒp0N2Œ1ãŽ¶sº×IÒäÑáªíŠ—hk¥ˆýª\—
+9k>|í¹º®àeðå>¹¾—7¶;êÕžÿ] h0:µyMXî~‘UØðM$HŽÎoÎ%(¶ü
+wwµ÷‹`Ëá·\¢“ºÿ#â%×Ðs6ÛyDBv&	éì˜S_xx²/…U5p€ž™¹UÎá*© L+¬BÈp"ÂØYÜs½«=!ÀÕài¡4ç¹³|
