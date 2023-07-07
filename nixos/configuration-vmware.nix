f4a32a8c11c9e86ebd86a972131bd6d264ee3fa5
Marin <git@mar.in>
2023-07-07 11:06:22 -0400

    add tokio-vm to syncthing and tailscale

--- nixos/configuration-vmware.nix
+++ nixos/configuration-vmware.nix
@@ -15,9 +15,10 @@
   services.getty.autologinUser = "marin";
   services.udisks2.enable = true; # needed for fwupdmgr -.-
   services.yubikey-agent.enable = true;
-  #services.syncthing = secrets.syncthing "tokio" // {
-  #  user = "marin";
-  #};
+  services.syncthing = secrets.syncthing "tokio-vm" // {
+    user = "marin";
+  };
+  services.tailscale.enable = true;
 
   users.users.marin = {
     shell = pkgs.fish;
