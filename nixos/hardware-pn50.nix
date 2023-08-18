fd947fcde4643a71c01750e229b0d3f2f0906643
Marin <git@mar.in>
2023-08-17 20:20:09 -0400

    whitespace

--- nixos/hardware-pn50.nix
+++ nixos/hardware-pn50.nix
@@ -4,20 +4,21 @@
 { config, lib, pkgs, modulesPath, ... }:
 
 {
-  imports = [ 
+  imports = [
     (modulesPath + "/installer/scan/not-detected.nix")
   ];
 
   boot.initrd.availableKernelModules = [ "xhci_pci" "ehci_pci" "nvme" "ahci" "usb_storage" "usbhid" "sd_mod" "rtsx_usb_sdmmc" ];
-  boot.initrd.luks.devices.cryptroot.device = "/dev/disk/by-uuid/671c146e-1a13-47a8-89c4-bcf8918deef8";
+  # boot.initrd.luks.devices.cryptroot.device = "/dev/disk/by-uuid/671c146e-1a13-47a8-89c4-bcf8918deef8";
+  boot.initrd.luks.devices.cryptroot.device = "/dev/mapper/cryptroot";
   boot.kernelModules = [ "kvm-amd" ];
 
-  fileSystems."/" = { 
+  fileSystems."/" = {
     device = "/dev/disk/by-uuid/00e8554d-251c-4884-9a67-c9ff934eb6fa";
     fsType = "ext4";
   };
 
-  fileSystems."/boot" = { 
+  fileSystems."/boot" = {
     device = "/dev/disk/by-uuid/B0FA-4FCB";
     fsType = "vfat";
   };
