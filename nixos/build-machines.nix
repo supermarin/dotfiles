1f47b1470177d625d8030edef452417f2c62580f
Marin <git@mar.in>
2023-09-05 22:18:00 -0400

    remove explicit sshUser
    looks like this just works with tailscale

--- nixos/build-machines.nix
+++ nixos/build-machines.nix
@@ -2,7 +2,6 @@
   {
     hostName = "pumba";
     system = "x86_64-linux";
-    sshUser = "marin";
     speedFactor = 10;
     supportedFeatures = [ "nixos-test" "benchmark" "big-parallel" "kvm" ];
   }
