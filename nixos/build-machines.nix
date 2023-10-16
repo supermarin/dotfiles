149d11670aee23013d6298550309aad67394ae9e
Marin <git@mar.in>
2023-10-16 12:39:09 -0400

    bai pumba

--- nixos/build-machines.nix
+++ nixos/build-machines.nix
@@ -1,6 +1,6 @@
 [
   {
-    hostName = "pumba";
+    hostName = "mx-001";
     system = "x86_64-linux";
     speedFactor = 10;
     supportedFeatures = [ "nixos-test" "benchmark" "big-parallel" "kvm" ];
