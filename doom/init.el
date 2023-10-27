99ab4e5b33424c55d4a6f95c4b9e5104ee2d7bb3
Marin <git@mar.in>
2023-10-26 20:26:58 -0400

    [doom] temp disable org
    Doesn't build, will tackle later if I ever use org again

--- doom/init.el
+++ doom/init.el
@@ -154,7 +154,7 @@
        ;;nim               ; python + lisp at the speed of c
        nix               ; I hereby declare "nix geht mehr!"
        ;;ocaml             ; an objective camel
-       org               ; organize your plain life in plain text
+       ;; org               ; organize your plain life in plain text
        ;;php               ; perl's insecure younger brother
        ;;plantuml          ; diagrams for confusing people more
        ;;purescript        ; javascript, but functional
@@ -183,7 +183,7 @@
        ;;(wanderlust +gmail)
 
        :app
-       calendar
+       ;; calendar
        ;;emms
        ;;everywhere        ; *leave* Emacs!? You must be joking
        ;;irc               ; how neckbeards socialize
