522323c44f8d378882d901d1fb2bfd7453f6b5fc
Marin <git@mar.in>
2024-02-13 18:04:22 -0500

    more fonts cleanup

--- nixos/configuration-vmware.nix
+++ nixos/configuration-vmware.nix
@@ -1,5 +1,7 @@
 { inputs, config, pkgs, secrets, ... }:
 {
+  imports = [ ./fonts.nix ];
+
   virtualisation.vmware.guest.enable = true;
   time.timeZone = "America/New_York";
 
@@ -56,30 +58,6 @@
         clipmenu
         xsel
       ];
-    };
-  };
-
-  fonts = {
-    enableDefaultPackages = false;
-    packages = with pkgs; [
-      (import ../fonts/sfpro.nix { pkgs = pkgs; }) # sans
-      (import ../fonts/sfmono.nix { pkgs = pkgs; }) # mono for browser
-      source-serif # serif
-      jetbrains-mono # mono for terminal and vim
-
-      noto-fonts-emoji # emoji
-      font-awesome # i3status-rust
-
-      ibm-plex # more mono ftw
-      hack-font # more mono ftw
-    ];
-    fontconfig = {
-      defaultFonts = {
-        serif = [ "Source Serif 4" ];
-        sansSerif = [ "SF Pro Display" ];
-        monospace = [ "SF Mono" ];
-        emoji = [ "Noto" ];
-      };
     };
   };
 
