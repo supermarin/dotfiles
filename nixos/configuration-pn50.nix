77d4b30d598f015348bc8c316c32f8c78b3c16c1
marin <marin@mx-001>
2024-02-16 18:03:54 -0500

    more syncthing settings

--- nixos/configuration-pn50.nix
+++ nixos/configuration-pn50.nix
@@ -89,19 +89,20 @@
       options.localAnnounceEnabled = false;
       options.relaysEnabled = false;
       options.natEnabled = false;
-    };
-    devices = {
-      simba.id = "HGXXVK4-TSKAZWM-XYLAEH5-USXQ3YV-76YM2RW-K4LTUPQ-FDWEN2I-IG2LDAR";
-      simba.addresses = [ "quic://simba" ];
-      tokio.id = "3R5ICHB-XN4DEI4-7NUMPI2-DCL24JI-3A2XN2Y-6IG6UTX-VXO22EL-66T3ZQM";
-      tokio.addresses = [ "quic://tokio" ];
-      mx-001.id = "4FNONH3-QTHSBAF-NKPXL4A-IDTEYGI-LKC2QN2-RXQ3UUT-CUQFWM6-JFCB7AU";
-      mx-001.addresses = [ "quic://mx-001" ];
-    };
-    folders = {
-      "~/base" = {
-        id = "ctyq6-lwqs6";
-        devices = [ "tokio" "simba" "mx-001" ];
+      options.urAccepted = -1;
+      devices = {
+        simba.id = "HGXXVK4-TSKAZWM-XYLAEH5-USXQ3YV-76YM2RW-K4LTUPQ-FDWEN2I-IG2LDAR";
+        simba.addresses = [ "quic://simba" ];
+        tokio.id = "3R5ICHB-XN4DEI4-7NUMPI2-DCL24JI-3A2XN2Y-6IG6UTX-VXO22EL-66T3ZQM";
+        tokio.addresses = [ "quic://tokio" ];
+        mx-001.id = "4FNONH3-QTHSBAF-NKPXL4A-IDTEYGI-LKC2QN2-RXQ3UUT-CUQFWM6-JFCB7AU";
+        mx-001.addresses = [ "quic://mx-001" ];
+      };
+      folders = {
+        "~/base" = {
+          id = "ctyq6-lwqs6";
+          devices = [ "tokio" "simba" "mx-001" ];
+        };
       };
     };
   };
