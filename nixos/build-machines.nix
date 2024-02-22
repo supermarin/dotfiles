5bfd0707623306a66e034c0321228e589f4b565f
Marin <git@mar.in>
2024-02-22 12:25:04 -0500

    add mufasa to build machines

--- nixos/build-machines.nix
+++ nixos/build-machines.nix
@@ -1,8 +1,14 @@
 [
   {
-    hostName = "mx-001";
+    hostName = "mufasa";
     system = "x86_64-linux";
     speedFactor = 10;
+    supportedFeatures = [ "nixos-test" "benchmark" "big-parallel" "kvm" ];
+  }
+  {
+    hostName = "mx-001";
+    system = "x86_64-linux";
+    speedFactor = 7;
     supportedFeatures = [ "nixos-test" "benchmark" "big-parallel" "kvm" ];
   }
 ]
