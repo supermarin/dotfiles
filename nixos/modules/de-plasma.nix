6f1de4b237ebc467cc8c0d6829f5d6d74359a868
Marin <git@mar.in>
2024-03-02 10:06:27 -0500

    whatever

--- nixos/modules/de-plasma.nix
+++ nixos/modules/de-plasma.nix
@@ -1,5 +1,11 @@
+{ pkgs, ... }:
 {
   services.xserver.enable = true;
   services.xserver.displayManager.sddm.enable = true;
   services.xserver.desktopManager.plasma5.enable = true;
+  environment.plasma5.excludePackages = with pkgs.libsForQt5; [
+    plasma-browser-integration
+    konsole
+    oxygen
+  ];
 }
