416a0a5e531e0df04d6e9c061cccd9ab5ea909fe
Marin <git@mar.in>
2024-03-25 11:02:33 -0400

    build machines v2

--- nixos/build-machines.nix
+++ nixos/build-machines.nix
@@ -1,14 +1,22 @@
-[
-  {
+host:
+
+let
+  mufasa = {
     hostName = "mufasa";
     system = "x86_64-linux";
     speedFactor = 10;
     supportedFeatures = [ "nixos-test" "benchmark" "big-parallel" "kvm" ];
-  }
-  {
+  };
+  mx-001 = {
     hostName = "mx-001";
     system = "x86_64-linux";
-    speedFactor = 7;
+    speedFactor = 6;
     supportedFeatures = [ "nixos-test" "benchmark" "big-parallel" "kvm" ];
-  }
-]
+  };
+in
+{
+  mufasa = [ ];
+  mx-001 = [ mufasa mx-001 ];
+  simba = [ mufasa mx-001 ];
+  tokio = [ mufasa mx-001 ];
+}.${host}
