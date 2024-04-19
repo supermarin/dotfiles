fa372f11c48465056d846f9c0cf38a7ae343c3c0
Marin <git@mar.in>
2024-04-19 08:58:33 -0400

    [kde] whatever

--- nixos/modules/de-kde.nix
+++ nixos/modules/de-kde.nix
@@ -1,8 +1,9 @@
-{ pkgs, ... }:
+{ pkgs, lib, ... }:
 {
   services.xserver.enable = true;
-  services.xserver.displayManager.sddm.enable = true;
+  services.xserver.displayManager.sddm.wayland.enable = true;
   services.desktopManager.plasma6.enable = true;
+  programs.gnupg.agent.pinentryPackage = lib.mkForce pkgs.pinentry-all;
   environment.plasma6.excludePackages = with pkgs.kdePackages; [
     plasma-browser-integration
     konsole
