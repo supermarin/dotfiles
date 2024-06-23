72ae13dd40d0657e491ffab8bf63163f90fd8258
Marin <git@mar.in>
2024-06-22 20:02:09 -0400

    [gnome] temp: swap_lalt_lwin
    This might break external keybards, which I don't have on me ATM

--- linux/gnome/dconf.nix
+++ linux/gnome/dconf.nix
@@ -11,7 +11,7 @@ with pkgs.lib.gvariant;
   #   terminal.exec = mkString "ghostty";
   # };
   "org/gnome/desktop/input-sources" = {
-    xkb-options = [ "terminate:ctrl_alt_bksp" "caps:ctrl_modifier" ];
+    xkb-options = [ "terminate:ctrl_alt_bksp" "caps:ctrl_modifier" "altwin:swap_lalt_lwin"];
   };
   "org/gnome/desktop/interface" = {
     color-scheme = "prefer-dark";
