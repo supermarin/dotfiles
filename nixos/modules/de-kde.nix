bd741b615b73ff823443ca6e0e4ec0b78535f1f2
Marin <git@mar.in>
2024-10-15 14:30:08 -0400

    [nixos][kde] explicitly enable sddm
    no idea why, didn't document.
    still not using KDE, was for testing XDR display.

--- nixos/modules/de-kde.nix
+++ nixos/modules/de-kde.nix
@@ -1,6 +1,7 @@
 { pkgs, lib, ... }:
 {
   services.xserver.enable = true;
+  services.xserver.displayManager.sddm.enable = true;
   services.xserver.displayManager.sddm.wayland.enable = true;
   services.desktopManager.plasma6.enable = true;
   programs.gnupg.agent.pinentryPackage = lib.mkForce pkgs.pinentry-all;
