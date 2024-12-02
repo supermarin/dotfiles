914b6ad4e7e9ec02162f5d0958814c2f04ba61c2
Marin <git@mar.in>
2024-12-02 15:18:11 -0500

    [nix] remove mx-001 from build build-machines

--- nixos/build-machines.nix
+++ nixos/build-machines.nix
@@ -7,16 +7,10 @@ let
     speedFactor = 10;
     supportedFeatures = [ "nixos-test" "benchmark" "big-parallel" "kvm" ];
   };
-  mx-001 = {
-    hostName = "mx-001";
-    system = "x86_64-linux";
-    speedFactor = 6;
-    supportedFeatures = [ "nixos-test" "benchmark" "big-parallel" "kvm" ];
-  };
 in
 {
   mufasa = [ ];
-  mx-001 = [ mufasa mx-001 ];
-  simba = [ mufasa mx-001 ];
-  tokio = [ mufasa mx-001 ];
+  mx-001 = [ mufasa ];
+  simba = [ mufasa ];
+  tokio = [ mufasa ];
 }.${host}
