8f3a2b466f58e4c7f9c0f004ae81e7cf6d6878c6
Marin <git@mar.in>
2024-12-03 14:02:33 -0500

    [flake] pin nixpkgs unstable properly
    looks like i have no idea what i was doing

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     nixos-hardware.url = "github:NixOS/nixos-hardware";
     home-manager = {
       url = "github:nix-community/home-manager";
