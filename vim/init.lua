d2c32bfb6c60034d50fbc83ad2b488f7c7ec892e
Marin <git@mar.in>
2024-12-04 16:45:05 -0500

    [vim] update delve dap

--- vim/init.lua
+++ vim/init.lua
@@ -370,19 +370,25 @@ require('dap-python').setup()
 local dap = require('dap')
 
 dap.adapters.delve = {
-  type = 'server',
-  port = '${port}',
-  executable = {
-    command = 'dlv',
-    args = { 'dap', '-l', '127.0.0.1:${port}' },
-  }
-}
-
-dap.configurations.go = {
+  {
+    type = 'server',
+    port = '${port}',
+    executable = {
+      command = 'dlv',
+      args = { 'dap', '-l', '127.0.0.1:${port}' },
+    },
+  },
   {
     type = "delve",
     name = "Debug file",
     request = "launch",
+    program = "${file}"
+  },
+  {
+    type = "delve",
+    name = "Debug test file",
+    request = "launch",
+    mode = "test",
     program = "${file}"
   },
   {
