ca72f9b31b141960fd17cadabaf16dd487ab1d57
Marin <git@mar.in>
2024-12-04 17:04:05 -0500

    fix lix

--- flake.nix
+++ flake.nix
@@ -42,7 +42,7 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
     lix = {
-      url = "https://git.lix.systems/lix-project/nixos-module/archive/2.91.0.tar.gz";
+      url = "https://git.lix.systems/lix-project/nixos-module/archive/2.91.1-2.tar.gz";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     steven-black-hosts = {
