1801346d11a1f5c7355bd8a0c062fea247127cff
Marin <git@mar.in>
2024-12-04 11:13:56 -0500

    [nixos] add nixbuild.net placeholder
    This worked, but think building things at home works well enough + cache

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -58,6 +58,19 @@
     stevenBlackHosts.enable = true; 
   };
   programs.fish.enable = true;
+  # programs.ssh.knownHosts = {
+  #   nixbuild = {
+  #     hostNames = [ "eu.nixbuild.net" ];
+  #     publicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPIQCZc54poJ8vqawd8TraNryQeJnvH1eLpIDgbiqymM";
+  #   };
+  # };
+  # programs.ssh.extraConfig = ''
+  #   Host eu.nixbuild.net
+  #   PubkeyAcceptedKeyTypes ssh-ed25519
+  #   ServerAliveInterval 60
+  #   IPQoS throughput
+  #   IdentityFile /etc/ssh/ssh_host_ed25519_key
+  # '';
 
   services.fwupd.enable = true;
   services.openssh.enable = true;
