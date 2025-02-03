7f2fa6729dc1d29ebc174e7e5bd95ee7fa5e8ad1
Marin <git@mar.in>
2025-02-03 12:15:15 -0500

    update kde settings

--- nixos/modules/de-kde.nix
+++ nixos/modules/de-kde.nix
@@ -1,10 +1,12 @@
-{ pkgs, lib, ... }:
-{
-  services.xserver.enable = true;
-  services.xserver.displayManager.sddm.enable = true;
-  services.xserver.displayManager.sddm.wayland.enable = true;
+{ pkgs, lib, ... }: {
+  imports = [ ./fonts.nix ];
+
+  services.displayManager.sddm.enable = true;
+  services.displayManager.sddm.wayland.enable = true;
   services.desktopManager.plasma6.enable = true;
+
   programs.gnupg.agent.pinentryPackage = lib.mkForce pkgs.pinentry-all;
+  environment.plasma6.enable = true;
   environment.plasma6.excludePackages = with pkgs.kdePackages; [
     plasma-browser-integration
     konsole
