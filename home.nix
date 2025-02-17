798c6686213542d52034262ed1eea91b47e84297
Marin <git@mar.in>
2025-02-17 14:48:17 -0500

    [home] temp remove hardcoded SSH_AUTH_SOCK

--- home.nix
+++ home.nix
@@ -23,7 +23,8 @@ in
     RIPGREP_CONFIG_PATH = "$HOME/.config/rg/config"; # TODO: XDG_CONFIG_HOME
     NIXOS_OZONE_WL = "1"; # https://discourse.nixos.org/t/partly-overriding-a-desktop-entry/20743/2
     AGE_RECIPIENTS_FILE = ./age/recipients.txt;
-    SSH_AUTH_SOCK = "$XDG_RUNTIME_DIR/yubikey-agent/yubikey-agent.sock";
+    # This breaks auth sock over ssh
+    # SSH_AUTH_SOCK = "$XDG_RUNTIME_DIR/yubikey-agent/yubikey-agent.sock";
   };
 
   home.sessionPath = [ "$HOME/dotfiles/functions" ];
