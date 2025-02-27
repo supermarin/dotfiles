faa4600ac8f8aca89f574cb4c4e0496e39512d0d
Marin <git@mar.in>
2025-02-27 16:27:46 -0500

    [fish] test for tmux first

--- fish/functions/wo.fish
+++ fish/functions/wo.fish
@@ -1,9 +1,14 @@
 #!/usr/bin/env fish
 
 set --local dir (find -L ~/code -maxdepth 2 | fzf)
+
 if [ $dir ]
   cd $dir
   clear
   la
+end
+
+if test $TMUX
+  # TODO: open new tab in tmux?
   tmux rename-window (basename $dir)
 end
