8ef5c8731261f737c325c4c5791652674ac5fc43
Marin <git@mar.in>
2025-02-27 16:25:17 -0500

    [fish] start removing some fish-only functions
    This is prep to potentially switch off of fish down the line

--- fish/fish.nix
+++ fish/fish.nix
@@ -4,17 +4,7 @@ pkgs: {
     if status --is-interactive
       abbr c clear
       abbr g git
-      abbr gd git d
-      abbr gl git pull
-      abbr gp git push
-      abbr gs git s
       abbr nb nix build
-      abbr nd nix develop
-      abbr nr nix run
-      abbr nrb sudo nixos-rebuild --flake $HOME/dotfiles#$(hostname) switch
-      abbr nrr nix run nixpkgs#
-      abbr ns nix shell nixpkgs#
-      abbr nsp nix-shell -p
       abbr nss nix search nixpkgs
     end
   '';
@@ -23,10 +13,5 @@ pkgs: {
   functions = with builtins; {
     wo = readFile ./functions/wo.fish;
     fish_right_prompt = readFile ./functions/fish_right_prompt.fish;
-    whatsapp = ''${pkgs.ungoogled-chromium}/bin/chromium --ozone-platform-hint=auto --force-dark-mode --enable-features=WebUIDarkMode --app="https://web.whatsapp.com"'';
-    remotevsc = ''${pkgs.ungoogled-chromium}/bin/chromium --ozone-platform-hint=auto --force-dark-mode --enable-features=WebUIDarkMode --app="https://mufasa.dingo-marlin.ts.net/?folder=%2Fhome%2Fmarin%2Fcode%2Fsquale-capital&tkn=04392cca-4c44-4753-8c0a-26b70ba3d161"'';
-    la = ''
-      eza --octal-permissions --long --all --git $argv
-    '';
   };
 }
