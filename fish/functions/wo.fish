d4330766d7255dac86c2407fb3ffe4429e6587d6
Marin <git@mar.in>
2025-03-14 17:45:36 -0400

    [wo][wip] better handling of tmux

--- fish/functions/wo.fish
+++ fish/functions/wo.fish
@@ -3,14 +3,17 @@
 function wo
   set --local dir (find -L ~/code -maxdepth 2 | fzf)
 
-  if [ $dir ]
-    cd $dir
-    clear
-    la
-
-    if test $TMUX
-      # TODO: open new tab in tmux?
-      tmux rename-window (basename $dir)
-    end
+  if ! [ $dir ]
+    return 1
   end
+
+  if test $TMUX
+    # TODO: open new tab in tmux?
+    tmux rename-window (basename $dir)
+  end
+
+  cd $dir
+  clear
+  la
 end
+
