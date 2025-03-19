15733abf79b1dc9d40213093183a3f1bb5c8219d
Marin <git@mar.in>
2025-03-19 12:04:28 -0400

    before i nuke all fish stuff, add bash stuff

--- shells.nix
+++ shells.nix
@@ -2,22 +2,39 @@
 {
   programs.bash = {
     enable = true;
+    # initExtra = ''
+    #   # Keep bash as login shell, but if running interactively, launch fish.
+    #   if [[ $(${pkgs.procps}/bin/ps --no-header --pid=$PPID --format=comm) != "fish" && -z ''${BASH_EXECUTION_STRING} ]]
+    #   then
+    #     shopt -q login_shell && LOGIN_OPTION='--login' || LOGIN_OPTION=""
+    #     exec ${pkgs.fish}/bin/fish $LOGIN_OPTION
+    #   fi
+    # '';
     initExtra = ''
-      # Keep bash as login shell, but if running interactively, launch fish.
-      if [[ $(${pkgs.procps}/bin/ps --no-header --pid=$PPID --format=comm) != "fish" && -z ''${BASH_EXECUTION_STRING} ]]
-      then
-        shopt -q login_shell && LOGIN_OPTION='--login' || LOGIN_OPTION=""
-        exec ${pkgs.fish}/bin/fish $LOGIN_OPTION
-      fi
+      wo() {
+        local dir=$(find -L ~/code -maxdepth 2 | fzf)
+        test $dir || return 1
+
+        if [[ -n "$TMUX" ]]; then
+          # TODO: open new tab in tmux?
+          tmux rename-window "$(basename "$dir")"
+        fi
+
+        cd "$dir" || return
+        clear
+        ls -la
+      }
+
+      eval "$(${pkgs.oh-my-posh}/bin/oh-my-posh init bash)"
     '';
   };
 
-  programs.fish = {
-    enable = true;
-    interactiveShellInit = ''
-      source ${./fish/functions/fish_prompt.fish}
-      source ${./fish/functions/fish_right_prompt.fish}
-      source ${./fish/functions/wo.fish}
-    '';
-  };
+  # programs.fish = {
+  #   enable = true;
+  #   interactiveShellInit = ''
+  #     source ${./fish/functions/fish_prompt.fish}
+  #     source ${./fish/functions/fish_right_prompt.fish}
+  #     source ${./fish/functions/wo.fish}
+  #   '';
+  # };
 }
