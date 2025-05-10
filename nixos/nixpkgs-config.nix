ee0718bce7d0764b25a2a02cfe8608575d81af88
Marin <git@mar.in>
2025-05-10 12:06:17 -0400

    turn off optimise
    not sure if this is the right call, revisit

--- nixos/nixpkgs-config.nix
+++ nixos/nixpkgs-config.nix
@@ -18,14 +18,6 @@
       experimental-features = nix-command flakes
       builders-use-substitutes = true
     '';
-    # gc = {
-    #   automatic = true;
-    #   dates = "monthly";
-    # };
-    optimise = {
-      automatic = true;
-      dates = [ "monthly" ];
-    };
     registry.nixpkgs.flake = inputs.nixpkgs;
     settings = {
       trusted-users = [ "marin" ]; # enable nix-copy-closure
