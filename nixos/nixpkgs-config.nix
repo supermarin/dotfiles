221b0e3c7ab122851743f5f8d2972d0284b69271
Marin <git@mar.in>
2025-05-12 21:19:28 -0400

    comments

--- nixos/nixpkgs-config.nix
+++ nixos/nixpkgs-config.nix
@@ -7,6 +7,7 @@
 {
   programs.nh = {
     enable = true;
+    # TODO: not sure if I should revert back to vanilla here
     clean.enable = true;
     clean.dates = "monthly";
     clean.extraArgs = "--keep-since 30d --keep 7";
@@ -18,9 +19,17 @@
       experimental-features = nix-command flakes
       builders-use-substitutes = true
     '';
+    # gc = {
+    #   automatic = true;
+    #   dates = "monthly";
+    # };
+    # optimise = {
+    #   automatic = true;
+    #   dates = [ "monthly" ];
+    # };
     registry.nixpkgs.flake = inputs.nixpkgs;
     settings = {
-      trusted-users = [ "marin" ]; # enable nix-copy-closure
+      trusted-users = [ "marin" ]; # nix-copy-closure. TODO: see if still needed
     };
   };
 }
