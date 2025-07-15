8e35971d20cb52b5d61e896f9b01680f49fed232
Marin <git@mar.in>
2025-07-15 16:42:03 -0400

    update kde settings

--- nixos/modules/de-kde.nix
+++ nixos/modules/de-kde.nix
@@ -7,8 +7,17 @@
   services.desktopManager.plasma6.enable = true;
 
   programs.gnupg.agent.pinentryPackage = lib.mkForce pkgs.pinentry-all;
+  programs.ssh.startAgent = true;
+  environment.systemPackages = with pkgs; [
+    ddcutil # another brightness control. for ext displays via i2c
+    kdePackages.ghostwriter
+    kdePackages.kcalc
+    kdePackages.kcontacts
+    kdePackages.merkuro
+
+    wl-clipboard
+  ];
   environment.plasma6.excludePackages = with pkgs.kdePackages; [
-    plasma-browser-integration
     oxygen
   ];
 }
