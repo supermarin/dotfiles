bece14c563e927b6d535205f569671e7e65358fd
Marin <git@mar.in>
2025-07-15 16:47:44 -0400

    add zen flake

--- flake.nix
+++ flake.nix
@@ -31,6 +31,10 @@
       url = "github:StevenBlack/hosts";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    zen-browser = {
+      url = "github:0xc000022070/zen-browser-flake";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs = inputs: {
