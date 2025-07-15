2ac4c776b947d8ee99158ccbc16fed59d2aa1330
Marin <git@mar.in>
2025-07-15 16:41:43 -0400

    update gnome xserver settings

--- nixos/modules/de-gnome.nix
+++ nixos/modules/de-gnome.nix
@@ -4,9 +4,8 @@
     ./fonts.nix
   ];
 
-  services.xserver.enable = true;
-  services.xserver.displayManager.gdm.enable = true;
-  services.xserver.desktopManager.gnome.enable = true;
+  services.displayManager.gdm.enable = true;
+  services.desktopManager.gnome.enable = true;
 
   environment.variables = {
     GNOME_SHELL_SLOWDOWN_FACTOR = "0.4"; # speed up animations
