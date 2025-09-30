72e44b2ceff3cf1d7986b624bb01b080c17a10ca
Marin <git@mar.in>
2025-09-30 14:41:26 -0600

    [cosmic] remove a bunch of settigs
    Now it's a part of nixpkgs

--- nixos/modules/de-cosmic.nix
+++ nixos/modules/de-cosmic.nix
@@ -1,16 +1,7 @@
-{ inputs, pkgs, ... }:
 {
   imports = [
-    inputs.nixos-cosmic.nixosModules.default
     ./fonts.nix
   ];
-  nix.settings = {
-    substituters = [ "https://cosmic.cachix.org/" ];
-    trusted-public-keys = [ "cosmic.cachix.org-1:Dya9IyXD4xdBehWjrkPv6rtxpmMdRel02smYzA85dPE=" ];
-  };
   services.desktopManager.cosmic.enable = true;
   services.displayManager.cosmic-greeter.enable = true;
-  environment.systemPackages = [
-    pkgs.cosmic-tasks
-  ];
 }
