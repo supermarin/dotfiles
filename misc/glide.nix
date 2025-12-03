5ef017b3d82ef2d0ac9fbe25c40df1a189b80745
Marin <git@mar.in>
2025-12-03 16:14:38 -0700

    update glide

--- misc/glide.nix
+++ misc/glide.nix
@@ -1,10 +1,10 @@
 { pkgs, ... }:
 pkgs.stdenv.mkDerivation rec {
   name = "Glide";
-  version = "0.1.51a";
+  version = "0.1.54a";
   src = pkgs.fetchurl {
     url = "https://github.com/glide-browser/glide/releases/download/${version}/glide.linux-x86_64.tar.xz";
-    sha256 = "sha256-eQ8itn8XvFNuLypKruTLpWyzDr899lLTMBU3yN+yGeU=";
+    sha256 = "sha256-Rw85b+9eaiM9szWpYZiF7FqJY7OpliOwt09/c8UWlGk=";
   };
   nativeBuildInputs = [ pkgs.makeWrapper ];
   installPhase = ''
