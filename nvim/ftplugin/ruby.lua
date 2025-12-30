f86587a43ca862b3bd6a06f5aa644ba6c0495c1b
Marin <git@mar.in>
2025-12-30 13:31:13 -0700

    [vim] add ruby mappings

--- nvim/ftplugin/ruby.lua
+++ nvim/ftplugin/ruby.lua
@@ -1,1 +1,3 @@
 vim.keymap.set('i', '<C-l>', ' => ', { noremap = true })
+vim.keymap.set('n', '<leader>r', ':w|:vs term://ruby %<cr>', { silent = true })
+vim.keymap.set('i', '<C-k>', '<Esc>d0idef <Esc>pA():\n', { noremap = true }) -- define method on the fly
